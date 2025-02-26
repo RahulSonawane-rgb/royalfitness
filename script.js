eba783df83aff6820918ac756b599a5a06db3025
RahulSonawane-rgb <sorahul54@gmail.com>
2025-02-26 11:22:14 +0530

    addes CSS and Assest JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//Typing Text Code
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Physical Fitness', 'Weight Gain', 'Strenght Training', 'Fat Loss', 'Weight Lifting', 'Running'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
+
